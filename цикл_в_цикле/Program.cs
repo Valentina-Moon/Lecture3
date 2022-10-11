61fe579bf92337da9df3e6180fba5af081913053
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-11 17:12:15 +0300

    таблица уножения

--- цикл_в_цикле/Program.cs
+++ цикл_в_цикле/Program.cs
@@ -1,11 +1,11 @@
 ﻿//таблица умножения.
 
-    for (int i = 2; i <10; i++)
-    {
-for (int j = 2; j <=10; i++)
+for (int i = 2; i < 10; i++)
 {
-    Console.WriteLine($"{i} x {j} = {i * j}");
-}
-    Console.WriteLine();
+    for (int j = 2; j <= 10; i++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
     }
+    Console.WriteLine();
+}
 
