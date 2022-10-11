e453c1c88280e81fe146c6e86fa3ad2f39143109
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-11 16:55:05 +0300

    применение for

---
+++ FOR/Program.cs
@@ -0,0 +1,16 @@
+﻿// пример с 4 видом
+
+string Method4(int count, string text)
+{
+
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+
+    {
+        result = result + text;
+
+    }
+    return result;
+}
+string res = Method4(10, "Z");
+Console.WriteLine(res);
